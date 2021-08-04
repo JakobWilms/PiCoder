93111f2689a1f716ca7d753fe9492617951fb2fe
Jakob Wilms <jakob.wilms06@t-online.de>
2021-08-04 15:04:53 +0200

    Commit

---
+++ src/main/bash/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+{
+  echo "Install? [Y|N]"
+  read -r install
+  if [ "$install" == "Y" ] || [ "$install" == "y" ]; then
+    cd /usr/share || (
+      echo "Couldn't find directory \"/usr/share\"!"
+      exit
+    )
+    sudo git clone https://github.com/JakobWilms/PiCoder || (
+      echo "Couldn't download sources. Is git installed?"
+      exit
+    )
+    cd PiCoder || (
+      echo "Downloaded sources not found. Please try again."
+      exit
+    )
+    sudo chmod +x src/main/bash/*
+  else
+    echo "Installation cancelled."
+    exit
+  fi
+} || {
+  echo "Something went wrong. Please check the following and try again!"
+  echo "OS = Linux"
+  echo "Git is installed"
+  echo "You are root"
+}
--- src/main/java/com/github/jakobwilms/picoder/initialize/Initializer.java
+++ src/main/java/com/github/jakobwilms/picoder/initialize/Initializer.java
@@ -1,12 +1,21 @@
 package com.github.jakobwilms.picoder.initialize;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
 import java.io.File;
 
 public class Initializer {
 
     public Initializer() {
-        File file = new File(File.pathSeparator + "usr" + File.pathSeparator + "bin", "picoder");
-        delete(file);
+        File file = new File(File.pathSeparator + "usr" + File.pathSeparator + "bin", "PiCoder");
+        if (file.exists())
+            delete(file);
+        try {
+            Git git = Git.cloneRepository().setURI("https://github.com/JakobWilms/PiCoder.git").setDirectory(new File("/usr/bin/PiCoder")).call();
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+        }
     }
 
     private void delete(File dir) {
