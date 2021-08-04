299ea50479d25b8530656fec0ce2a17f2d5a6c8b
Jakob Wilms <jakob.wilms06@t-online.de>
2021-08-04 17:15:14 +0200

    PiCoder

--- src/main/java/com/github/jakobwilms/picoder/key/KeyGenerator.java
+++ src/main/java/com/github/jakobwilms/picoder/key/KeyGenerator.java
@@ -46,7 +46,7 @@ public class KeyGenerator {
         FileOutputStream stream = new FileOutputStream(getFile());
         byte[] bytes = key.getBytes();
         for (int i = 0; i < bytes.length; i++) {
-            bytes[i] = (byte) (bytes[i] + 1);
+            bytes[i] = (byte) (bytes[i]* 2 + 1);
         }
         stream.write(bytes);
         stream.close();
